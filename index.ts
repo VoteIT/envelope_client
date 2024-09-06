c7cfec705ca26c0f49210d17cd61e4dabbd627a7
Johan Schiff <31409176+schyffel@users.noreply.github.com>
2024-09-06 10:37:36 +0200

    Update index.ts
    type export fix

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 export { default as Socket } from './src/Socket'
-export { SocketOptions } from './src/types'
+export type { SocketOptions } from './src/types'
 export { parseSocketError, isValidationError } from './src/errors'
 export { default as ProgressPromise } from './src/ProgressPromise'
 export * as types from './src/types'
